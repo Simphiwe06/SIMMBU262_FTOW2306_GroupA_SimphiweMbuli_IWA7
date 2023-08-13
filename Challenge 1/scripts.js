b4715779d1ce8317ffaf2a807d9807e37a546341
Simphiwe06 <simphiwembuli06@gmail.com>
2023-08-13 22:47:08 +0200

    changed the value  to a numerical value to log 10.

---
+++ Challenge 1/scripts.js
@@ -0,0 +1,4 @@
+const value = 3 // This number should be a numerical value, not a string.
+console.log(value + 4 + value)
+
+//JavaScript combined the values as strings, resulting in "343" instead of performing numeric addition beacuse it prioritizes string concatenation
